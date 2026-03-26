0071610ea99b86f016fa18a90bba167897010a1e
HEMACHARANREDDY <2310030419@klh.edu.in>
2026-03-26 19:22:44 +0530

    Fix: Remove redirect_uri from OAuth registration to use dynamic fallback

--- app.py
+++ app.py
@@ -175,7 +175,6 @@ google = oauth.register(
     client_id=os.environ.get('GOOGLE_CLIENT_ID', ''),
     client_secret=os.environ.get('GOOGLE_CLIENT_SECRET', ''),
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    redirect_uri=os.environ.get('REDIRECT_URI'),
     client_kwargs={'scope': 'openid email profile'},
 )
 
